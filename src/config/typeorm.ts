805f48cac3eada34d4617b76d653f1db3879ce42
sebademasi10 <sebademasi10@gmail.com>
2022-02-09 10:11:28 -0300

    Imprimo en consola parámetros de conexión a DB

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,6 +4,10 @@ import path from 'path';
 import { environment } from './environment';
 
 export async function connect() {
+    console.log('Parámetros de conexión a base de datos:')
+    console.log('DB_USERNAME:', environment.DB_USERNAME)
+    console.log('DB_PASSWORD:', environment.DB_PASSWORD)
+    console.log('DB_DATABASE:', environment.DB_DATABASE)
     await createConnection({
         type: 'postgres',
         port: Number(environment.DB_PORT),
